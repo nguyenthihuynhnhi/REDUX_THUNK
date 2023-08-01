70e06f2b1f673c2e1d4a57b165ba3020e032f90b
NGUYENNHI <huynhnhinguyen0301@gmail.com>
2023-08-01 21:07:57 +0700

    REDUX THUNK

---
+++ src/redux/reducers/userReducer.jsx
@@ -0,0 +1,18 @@
+const initialState = {
+  userLogin: null,
+};
+
+export const userReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "SAVE_USER_LOGIN": {
+      const copyState = JSON.parse(JSON.stringify(state));
+
+      copyState.userLogin = payload;
+      
+      return { ...state, userLogin: copyState.userLogin };
+    }
+
+    default:
+      return state;
+  }
+};
